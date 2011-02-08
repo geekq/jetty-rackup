0de47a4e2b83c7df4439585c54abeda32dffb63f
Leandro Silva <leandrodoze@gmail.com>
2011-02-08 20:56:18 -0200

    some few refactoring in the example app and an advice about auto reload

--- examples/app.rb
+++ examples/app.rb
@@ -1,5 +1,15 @@
+require 'sinatra'
+
+# If you want to have auto reload in development mode (what is sweet)
+# you can uncomment this following lines:
+#
+# configure :development do
+#   Sinatra::Application.reset!  # to reload routes and its contents
+#   use Rack::Reloader           # to reload required every files
+# end
+
 get '/?' do
-  'hello'
+  "hello"
 end
 
 get '/:message/?' do |message|
