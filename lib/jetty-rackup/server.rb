eec836b3341e8f0e6f23e85f75d9ea1b174ea47e
Nik Hodgkinson <nik@metamarketsgroup.com>
2012-01-01 23:08:00 -0800

    Migrated to Jetty 7.

--- lib/jetty-rackup/server.rb
+++ lib/jetty-rackup/server.rb
@@ -3,37 +3,36 @@ class Rack::Handler::Jetty
     Dir["#{File.dirname(__FILE__)}/../../jars/*.jar"].each { |jar| require jar }
 
     include_class 'javax.servlet.http.HttpServlet'
-    include_class 'org.mortbay.jetty.Server'
-    include_class 'org.mortbay.jetty.servlet.Context'
-    include_class 'org.mortbay.jetty.servlet.ServletHolder'
+    include_class 'org.eclipse.jetty.server.Server'
+    include_class 'org.eclipse.jetty.servlet.ServletContextHandler'
+    include_class 'org.eclipse.jetty.servlet.ServletHolder'
     include_class 'org.jruby.rack.servlet.ServletRackContext'
-    include_class 'org.mortbay.jetty.handler.ResourceHandler'
-    include_class 'org.mortbay.jetty.handler.DefaultHandler'
-    include_class 'org.mortbay.jetty.handler.HandlerList'
-    include_class 'org.mortbay.jetty.handler.ContextHandlerCollection'
-    include_class 'org.mortbay.jetty.servlet.DefaultServlet'
+    include_class 'org.eclipse.jetty.server.handler.ResourceHandler'
+    include_class 'org.eclipse.jetty.server.handler.DefaultHandler'
+    include_class 'org.eclipse.jetty.server.handler.HandlerList'
+    include_class 'org.eclipse.jetty.server.handler.ContextHandlerCollection'
+    include_class 'org.eclipse.jetty.servlet.DefaultServlet'
 
-    jetty = org.mortbay.jetty.Server.new options[:Port]
+    jetty = org.eclipse.jetty.server.Server.new options[:Port]
 
-    context = org.mortbay.jetty.servlet.Context.new(nil, "/", org.mortbay.jetty.servlet.Context::NO_SESSIONS)
-    context.add_filter("org.jruby.rack.RackFilter", "/*", org.mortbay.jetty.Handler::DEFAULT)
+    context = org.eclipse.jetty.servlet.ServletContextHandler.new(nil, "/", org.eclipse.jetty.servlet.ServletContextHandler::NO_SESSIONS)
+    context.add_filter("org.jruby.rack.RackFilter", "/*", 0)
     context.set_resource_base(File.dirname(__FILE__))
     context.add_event_listener(org.jruby.rack.RackServletContextListener.new)
 
-    context.set_init_params(java.util.HashMap.new(
-      'org.mortbay.jetty.servlet.Default.relativeResourceBase' => '/public',
-      'rackup' => rackup_content,
-      'jruby.max.runtimes' => '1'))
-      
-    context.add_servlet(org.mortbay.jetty.servlet.ServletHolder.new(
-      org.mortbay.jetty.servlet.DefaultServlet.new), "/")
-    
+    context.set_init_parameter('org.eclipse.jetty.servlet.Default.relativeResourceBase', '/public')
+    context.set_init_parameter('rackup', rackup_content)
+    context.set_init_parameter('jruby.max.runtimes', '1')
+
+    context.add_servlet(org.eclipse.jetty.servlet.ServletHolder.new(
+                            org.eclipse.jetty.servlet.DefaultServlet.new), "/")
+
     JRuby.runtime.jruby_class_loader.add_url(java.io.File.new("WEB-INF/classes").to_url)
 
     Dir["WEB-INF/lib/**/*.jar"].each do |jar|
       JRuby.runtime.jruby_class_loader.add_url(java.io.File.new(jar).to_url)
     end
-    
+
     jetty.set_handler(context)
     jetty.start
   end
