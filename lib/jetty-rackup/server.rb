d7f4acaef40da3f1cbd0d793f3c46c8283bbcc7c
erdeszt <erdeszt@gmail.com>
2013-02-07 21:38:24 +0100

    Changing the constructor call to jetty.server.Server to use the InetSocketAddress form to be able to use the host parameter (-o)

--- lib/jetty-rackup/server.rb
+++ lib/jetty-rackup/server.rb
@@ -2,6 +2,8 @@ class Rack::Handler::Jetty
   def self.run(rackup_content, options={})
     Dir["#{File.dirname(__FILE__)}/../../jars/*.jar"].each { |jar| require jar }
 
+    include_class 'java.net.InetAddress'
+    include_class 'java.net.InetSocketAddress'
     include_class 'javax.servlet.http.HttpServlet'
     include_class 'org.eclipse.jetty.server.Server'
     include_class 'org.eclipse.jetty.servlet.ServletContextHandler'
@@ -13,7 +15,7 @@ class Rack::Handler::Jetty
     include_class 'org.eclipse.jetty.server.handler.ContextHandlerCollection'
     include_class 'org.eclipse.jetty.servlet.DefaultServlet'
 
-    jetty = org.eclipse.jetty.server.Server.new options[:Port]
+    jetty = org.eclipse.jetty.server.Server.new(java.net.InetSocketAddress.new(InetAddress.getByName(options[:Host]), options[:Port]))
 
     context = org.eclipse.jetty.servlet.ServletContextHandler.new(nil, "/", org.eclipse.jetty.servlet.ServletContextHandler::NO_SESSIONS)
     context.add_filter("org.jruby.rack.RackFilter", "/*", 0)
